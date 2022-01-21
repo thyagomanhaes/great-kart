23ea420d0ce6cee767872a2275ca01a4f87b146b
Thyago Manhães <thyago.ufs@gmail.com>
2022-01-20 21:09:33 -0300

    Versão funcional com paginação na página de store

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from unicodedata import category
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
@@ -10,14 +12,21 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
-
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count
     }
     return render(request, 'store/store.html', context)
